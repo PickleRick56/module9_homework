6be98a121bfff64ce4fb4fa3e4cf8cb51768ccf2
PickleRick56 <36300782+PickleRick56@users.noreply.github.com>
2023-02-25 01:17:02 +0500

    Update task5.js

--- task5.js
+++ task5.js
@@ -1,9 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("myCat") !== null) {
+    let lastSession = localStorage
+      .getItem("myCat")
+      .split(null)
+      .filter((value) => {
+        return value !== "";
+      });
+    // lastSession=lastSession[0].split(',');
+    console.log(lastSession);
+    for (let a of lastSession) {
+      const img = document.createElement("img");
+      console.log(a);
+      img.src = a;
+      img.style.height = "100px";
+      img.style.width = "200px";
+      document.body.appendChild(img);
+    }
+  }
+});
+
 const button = document.querySelector("button");
 
 button.addEventListener("click", () => {
   const val1 = document.querySelector("#number1").value;
   const val2 = document.querySelector("#number2").value;
-  
 
   if (
     ((val1 < 1 || val1 > 10) && (val2 < 1 || val2 > 10)) ||
@@ -32,10 +52,17 @@ button.addEventListener("click", () => {
     })
     .then((data) => {
       console.log(data);
+      localStorage.removeItem("myCat");
       for (let a of data) {
-        console.log(a.url);
+        console.log(a.download_url);
+        localStorage.setItem(
+          "myCat",
+          `${localStorage.getItem("myCat") + a.download_url}null`
+        );
         const img = document.createElement("img");
-        img.src = a.url;
+        img.src = a.download_url;
+        img.style.height = "500px";
+        img.style.width = "500px";
         document.body.appendChild(img);
       }
     })
