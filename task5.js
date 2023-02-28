02a634324cf116bef8f4f0a094277418575bef02
PickleRick56 <36300782+PickleRick56@users.noreply.github.com>
2023-02-28 20:38:39 +0500

    Update task5.js

--- task5.js
+++ task5.js
@@ -1,19 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   if (localStorage.getItem("myCat") !== null) {
-    let lastSession = localStorage
-      .getItem("myCat")
-      .split(null)
-      .filter((value) => {
-        return value !== "";
-      });
-    // lastSession=lastSession[0].split(',');
-    console.log(lastSession);
-    for (let a of lastSession) {
+    const storedLinks = localStorage.getItem("myCat").split(" ");
+    for (const link of storedLinks) {
       const img = document.createElement("img");
-      console.log(a);
-      img.src = a;
-      img.style.height = "100px";
-      img.style.width = "200px";
+      img.src = link;
+      img.width = 100;
       document.body.appendChild(img);
     }
   }
@@ -25,48 +16,44 @@ button.addEventListener("click", () => {
   const val1 = document.querySelector("#number1").value;
   const val2 = document.querySelector("#number2").value;
 
-  if (
-    ((val1 < 1 || val1 > 10) && (val2 < 1 || val2 > 10)) ||
-    (isNaN(val1) && isNaN(val2))
-  ) {
-    console.log("Номер страницы и лимит вне диапазона от 1 до 10");
-    return;
+  const pageValid = val1 < 11 && val1 > 0;
+  const limitValid = val2 < 11 && val2 > 0;
+  const isValid = pageValid && limitValid;
+
+  if (isValid) {
+    fetch(`https://picsum.photos/v2/list?page=${val1}&limit=${val2}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const allImages = document.querySelectorAll("img");
+        for (const image of allImages) {
+          document.body.removeChild(image);
+        }
+
+        localStorage.removeItem("myCat");
+        for (const { download_url } of data) {
+          const storedLinks = localStorage.getItem("myCat");
+          localStorage.setItem(
+            "myCat",
+            storedLinks !== null
+              ? `${localStorage.getItem("myCat")} ${download_url}`
+              : download_url
+          );
+          const img = document.createElement("img");
+          img.src = download_url;
+          img.width = 100;
+          document.body.appendChild(img);
+        }
+      })
+      .catch((e) => {
+        console.log("error", e);
+      });
+  } else {
+    if (!pageValid) {
+      console.log("Номер страницы вне диапазона от 1 до 10");
+    }
+
+    if (!pageValid) {
+      console.log("Лимит вне диапазона от 1 до 10");
+    }
   }
-
-  if (val1 < 1 || val1 > 10 || isNaN(val1)) {
-    console.log("Номер страницы вне диапазона от 1 до 10");
-    return;
-  }
-
-  if (val2 < 1 || val2 > 10 || isNaN(val2)) {
-    console.log("Лимит вне диапазона от 1 до 10");
-    return;
-  }
-
-  fetch(`https://picsum.photos/v2/list?page=${val1}&limit=${val2}`)
-    .then((response) => {
-      console.log(response);
-      return response.json();
-
-      // document.querySelector("img").src = response.url;
-    })
-    .then((data) => {
-      console.log(data);
-      localStorage.removeItem("myCat");
-      for (let a of data) {
-        console.log(a.download_url);
-        localStorage.setItem(
-          "myCat",
-          `${localStorage.getItem("myCat") + a.download_url}null`
-        );
-        const img = document.createElement("img");
-        img.src = a.download_url;
-        img.style.height = "500px";
-        img.style.width = "500px";
-        document.body.appendChild(img);
-      }
-    })
-    .catch(() => {
-      console.log("error");
-    });
 });
